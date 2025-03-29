8d5c64ef1c5c5f58bfe0735d2159231a0fcd87c3
gibbontech <yasboss2412@gmail.com>
2025-03-29 11:07:38 +0100

    hey

--- srcs/exec/pipeline_utils/pipeline_utils2.c
+++ srcs/exec/pipeline_utils/pipeline_utils2.c
@@ -28,10 +28,48 @@ void	ft_prepare_pipe_io(int pipe_fd[2], int prev_pipe_fd, int i,
 	}
 }
 
+static char	**ft_create_unquoted_args(t_command *cmd)
+{
+	char	**unquoted_args;
+	int		i;
+
+	i = 0;
+	while (cmd->cmd_args[i])
+		i++;
+	unquoted_args = malloc(sizeof(char *) * (i + 1));
+	if (!unquoted_args)
+		return (NULL);
+	i = 0;
+	while (cmd->cmd_args[i])
+	{
+		unquoted_args[i] = ft_remove_quotes_from_str(cmd->cmd_args[i]);
+		if (!unquoted_args[i])
+			unquoted_args[i] = ft_strdup(cmd->cmd_args[i]);
+		i++;
+	}
+	unquoted_args[i] = NULL;
+	return (unquoted_args);
+}
+
+static void	ft_handle_exec_error_child(t_command *cmd, char *cmd_path,
+		char **env_array, char **unquoted_args)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(cmd->cmd_name, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(strerror(errno), 2);
+	ft_putstr_fd("\n", 2);
+	ft_free_table(unquoted_args);
+	ft_free_env_array(env_array, -1);
+	free(cmd_path);
+	exit(1);
+}
+
 void	ft_execute_child_command(t_command *cmd, char *cmd_path,
 		t_minishell *minishell)
 {
 	char	**env_array;
+	char	**unquoted_args;
 
 	env_array = ft_env_to_array(minishell);
 	if (!env_array)
@@ -39,16 +77,15 @@ void	ft_execute_child_command(t_command *cmd, char *cmd_path,
 		free(cmd_path);
 		exit(1);
 	}
-	ft_setup_child_signals();
-	if (execve(cmd_path, cmd->cmd_args, env_array) == -1)
+	unquoted_args = ft_create_unquoted_args(cmd);
+	if (!unquoted_args)
 	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(cmd->cmd_name, 2);
-		ft_putstr_fd(": ", 2);
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putstr_fd("\n", 2);
 		ft_free_env_array(env_array, -1);
 		free(cmd_path);
 		exit(1);
 	}
+	ft_setup_child_signals();
+	if (execve(cmd_path, unquoted_args, env_array) == -1)
+		ft_handle_exec_error_child(cmd, cmd_path, env_array, unquoted_args);
+	ft_free_table(unquoted_args);
 }
